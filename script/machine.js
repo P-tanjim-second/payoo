bb613d4c244105a252bf8aed0a0cb4da062f85bc
P-Tanjim <moksedurtanjim27@gmail.com>
2026-02-21 03:29:26 +0600

    add button click to open section function and add some active button functionality

--- script/machine.js
+++ script/machine.js
@@ -13,3 +13,30 @@ function checkPin(givenPin) {
     const correctPin = '1234';
     return givenPin === correctPin;
 }
+
+
+// show the section that user clicked and hide the other section
+const section = document.querySelectorAll('section');
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        if (!button.classList.contains("bg-primary")) {
+            buttons.forEach(btn => {
+                btn.classList.remove('bg-primary');
+                btn.classList.remove('text-white');
+            });
+
+        }
+        button.classList.add('bg-primary');
+        button.classList.add('text-white');
+        if (section[index + 2].classList.contains('hidden')) {
+            section[index + 2].classList.remove('hidden');
+            section.forEach((sec) => {
+                if (sec !== section[index + 2] && sec !== section[0] && sec !== section[1]) {
+                    sec.classList.add('hidden');
+                }
+            })
+        };
+    });
+});
